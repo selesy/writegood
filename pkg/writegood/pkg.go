758e73e7201fde30b6ffbd8bd75f7b19b53334f0
Steve Moyer <smoyer1@selesy.com>
2020-01-01 21:49:23 -0500

    Starts the beginning of the main go docs

---
+++ pkg/writegood/pkg.go
@@ -0,0 +1,23 @@
+/*
+Package writegood is a Go implementation of the write-good and write-
+gooder NPM modules (see https://github.com/duereg/write-gooder and
+https://github.com/btford/write-good respectively).
+
+
+External Tests
+
+	"adverb-where": "^0.2.1", - https://github.com/duereg/adverb-where
+	"e-prime": "^0.10.2", - https://github.com/Vorror/e-prime#readme
+	"no-cliches": "^0.2.2",- https://github.com/duereg/no-cliches
+	"passive-voice": "^0.1.0", - https://www.npmjs.com/package/passive-voice
+	"too-wordy": "^0.2.2", - https://github.com/duereg/too-wordy
+	"weasel-words": "^0.1.1" - https://www.npmjs.com/package/weasel-words
+	http://matt.might.net/articles/shell-scripts-for-weasel-words-weasel-words-duplicates/
+
+Internal tests
+
+	lexicalillusions
+	startswithso
+	thereis
+*/
+package writegood
